05c11b1b501117ffba6ba97a1e5c78b3fbc3ee08
Luke Freeman <lfreeman6490@gmail.com>
2021-07-26 21:17:10 -0500

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -11,9 +11,18 @@ void passing_in_params(string name, int age) {
     cout << "Hi " << name << " you are " << age << endl;
 }
 
+double cube_a_number(double number) {
+    return number * number * number;
+}
+
 int main()
 {
     //functions
     speak_to_user();
     passing_in_params("Jason", 50);
+    double answer = cube_a_number(30);
+    
+    cout << answer;
+    
+    return 0;
 }
