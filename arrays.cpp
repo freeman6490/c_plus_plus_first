0635db8aadce51c04a58eda66936ef757641eee0
Luke Freeman <lfreeman6490@gmail.com>
2021-07-26 21:05:46 -0500

    Create arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //arrays
+    double double_array[20] = {0,5,2,7,45.7,12.3,0,56.8};
+
+    cout << double_array[4] << endl;
+
+    double_array[0] = 19;
+
+    cout << double_array[0] << endl;
+
+}
