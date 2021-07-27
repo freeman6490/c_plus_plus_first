fed4442ad8936af37486a24d2ea30e3183624653
Luke Freeman <lfreeman6490@gmail.com>
2021-07-26 20:24:10 -0500

    Create first_variables.cpp
    first variables used

---
+++ first_variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name_of_person = "Luke";
+
+    int age;
+    age = 21;
+
+    cout << "There once was a man named " << name_of_person << endl;
+    cout << "He was " << age << " years old" << endl;
+    cout << "He liked the name " << name_of_person << endl;
+    cout << "And enjoyed being " << age << endl;
+    return 0;
+}
