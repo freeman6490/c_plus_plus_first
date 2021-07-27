045447ebac6740153f7ee09c0e6dad2061c56f23
Luke Freeman <lfreeman6490@gmail.com>
2021-07-26 20:49:18 -0500

    Create math_functions.cpp

---
+++ math_functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << pow(2, 6) << endl;
+    cout << pow(2, 9.32) << endl;
+
+    cout << sqrt(67) << endl;
+
+    cout << round(4.6593) << endl;
+
+    cout << floor(6.87) << endl;
+    cout << ceil(2.0002) << endl;
+
+    cout << fmax(2, 6) << endl;
+    cout << fmin(2, 6) << endl;
+}
