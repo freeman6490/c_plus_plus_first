eaf0911bc51dc4ee4e970c1e1e6f3c373fddeafe
Luke Freeman <lfreeman6490@gmail.com>
2021-08-08 21:23:34 -0500

    Create for_loops.cpp

---
+++ for_loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int index = 1;
+
+    while (index <= 5) {
+        cout << index << endl;
+        index++;
+    }
+
+    for(int i = 0; i <= 5; i++) {
+        cout << i << endl;
+    }
+    return 0;
+}
